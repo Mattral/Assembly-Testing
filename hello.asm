8e6d7ace378d10bc57829b4a430798fe53d48968
Min Htet Myet (Mattral) <88831350+Mattral@users.noreply.github.com>
2024-02-26 20:23:42 +0630

    Update hello.asm

--- hello.asm
+++ hello.asm
@@ -1,5 +1,4 @@
    org  0x100        ; .com files always start 256 bytes into the segment
-
     ; int 21h is going to want...
 
     mov  dx, msg      ; the address of or message in dx
@@ -8,6 +7,4 @@
 
     mov  ah, 0x4c     ; "terminate program" sub-function
     int  0x21         ; call dos services
-
     msg  db 'Hello, World!', 0x0d, 0x0a, '$'   ; $-terminated message
-
